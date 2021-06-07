549285538d74cad45cb815632916171ce55885f7
musangtara <musangtara@gmail.com>
2021-06-07 13:26:21 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -14,17 +14,17 @@ def create_folder(foldername):
 		p = pathlib.Path(foldername).mkdir(parents=True)
 	except FileExistsError:
 		out.error("There is already folder with that name!")
-
-def create_nvn_file(filename, config=None):
-	nvn = open(f"{foldername}/{filename}", "w")
+print("ter")
+def create_nvn_project():
+	nvn_file = open("")
+def create_nvn_file():
 	nvn_files = ["config", "plugins", "cache", "map", "script"]
 	nvn_folder = f"{foldername}/.nvnfile/"
 	nvn_extension = ".nvn.json"
-	if config == "nvn_files":
-		for i in range(len(nvn_files)):
-			nvn_files_content = '{\n\t"name": ' + '"' + nvn_files[i] + '"' + '\n}'
-			f = open(f"{nvn_folder}{nvn_files[i]}{nvn_extension}", "w")
-			f.write(nvn_files_content)
-			f.close()
+	for i in range(len(nvn_files)):
+		nvn_files_content = '{\n\t"name": ' + '"' + nvn_files[i] + '"' + '\n}'
+		f = open(f"{nvn_folder}{nvn_files[i]}{nvn_extension}", "w")
+		f.write(nvn_files_content)
+		f.close()
 
 create_nvn_file("nvn.json", config="nvn_files")
