63dfa82b6e34020570d8eeebab1bc2c03e77790c
musangtara <musangtara@gmail.com>
2021-06-07 13:30:20 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -4,23 +4,16 @@ import out
 foldername = input("Project Name: ")
 nvn_files = ["config", "plugins", "cache", "map", "script"]
 nvn_folder = f"{foldername}/.nvnfile/"
-
-try:
-	p = pathlib.Path(foldername).mkdir(parents=True)
-except FileExistsError:
-	out.error("There is already folder with that name!")
-
-p = pathlib.Path(foldername + "/" + ".nvnfile").mkdir(parents=True, exist_ok=True)
-
+nvn_extesion = ".nvn.json"
 def create_folder(foldername):
 	try:
 		p = pathlib.Path(foldername).mkdir(parents=True)
 	except FileExistsError:
 		out.error("There is already folder with that name!")
-print("ter")
 
 def create_nvn_config():
-
+	f = open("nvn.json", "w")
+	f.write("")
 def create_nvn_file():
 	for i in range(len(nvn_files)):
 		nvn_files_content = '{\n\t"name": ' + '"' + nvn_files[i] + '"' + '\n}'
