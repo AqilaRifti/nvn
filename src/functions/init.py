66942bc27cf840508892947977c4ac8045b19db2
musangtara <musangtara@gmail.com>
2021-06-07 12:50:55 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -1,7 +1,9 @@
 import os, sys, pathlib
 
 foldername = input("Project Name: ")
+
 p = pathlib.Path(foldername).mkdir(parents=True, exist_ok=True)
+p = pathlib.Path(foldername + "/" + ".nvnfile").mkdir(parents=True, exist_ok=True)
 f = open(f"{foldername}/config.py", "w")
 f.write('{"name": "nvn","version": 1.0,"command": {}}')
 f.close()
