fdba1ff176c9842caa2177af396eccb257a3084f
musangtara <musangtara@gmail.com>
2021-06-07 12:58:30 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -2,8 +2,11 @@ import os, sys, pathlib
 
 foldername = input("Project Name: ")
 
-p = pathlib.Path(foldername).mkdir(parents=True, exist_ok=True)
+p = pathlib.Path(foldername).mkdir(parents=True)
 p = pathlib.Path(foldername + "/" + ".nvnfile").mkdir(parents=True, exist_ok=True)
+
+def create_folder(foldername):
+	p = pathlib.Path(foldername).mkdir(parents=True, exist_ok=True)
 
 nvn_json = open(f"{foldername}/nvn.json", "w")
 nvn_config = open(f"{foldername}/.nvnfile/config.nvn.json", "w").write('{\n\t"name": "config"\n}')
