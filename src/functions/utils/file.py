34fe59c04b18d03c23723aefa41a070d9c5b17b2
musangtara <musangtara@gmail.com>
2021-06-07 14:43:22 +0700

    initial commit

---
+++ src/functions/utils/file.py
@@ -0,0 +1,8 @@
+import pathlib, out
+
+def create_folder(foldername):
+	try:
+		folder = pathlib.Path(foldername).mkdir(parents=True)
+	except FileExistsError:
+		out.error("There is already folder with that name!")
+		quit()
