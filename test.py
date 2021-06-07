2f650fa9e6fff5bf7678532866fb02ae242e9384
musangtara <musangtara@gmail.com>
2021-06-07 11:05:08 +0700

    initial commit

--- test.py
+++ test.py
@@ -1,20 +1,7 @@
-
 import requests
-import re
-
-def getFilename_fromCd(cd):
-	"""
-	Get filename from content-disposition
-	"""
-	if not cd:
-		return None
-	fname = re.findall('filename=(.+)', cd)
-	if len(fname) == 0:
-		return None
-	return fname[0]
 
 
-url = 'https://i.ytimg.com/an_webp/79Q4yZuFDyk/mqdefault_6s.webp?du=3000&sqp=CMqQ9oUG&rs=AOn4CLDEsSStiGTTIJzzIERUgmJBlNUtsw'
+url = 'https://www.facebook.com/favicon.ico'
 r = requests.get(url, allow_redirects=True)
-open("hello.png", 'wb').write(r.content)
-print(r.content)
+
+f = open('facebook.ico', 'wb').write(r.content)
