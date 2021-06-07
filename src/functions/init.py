fcd722c4ea4c920224b484f759e04098d7c9d0a9
musangtara <musangtara@gmail.com>
2021-06-07 13:30:56 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -4,7 +4,6 @@ import out
 foldername = input("Project Name: ")
 nvn_files = ["config", "plugins", "cache", "map", "script"]
 nvn_folder = f"{foldername}/.nvnfile/"
-nvn_extesion = ".nvn.json"
 def create_folder(foldername):
 	try:
 		p = pathlib.Path(foldername).mkdir(parents=True)
@@ -14,7 +13,9 @@ def create_folder(foldername):
 def create_nvn_config():
 	f = open("nvn.json", "w")
 	f.write("")
+
 def create_nvn_file():
+	nvn_extension = ".nvn.json"
 	for i in range(len(nvn_files)):
 		nvn_files_content = '{\n\t"name": ' + '"' + nvn_files[i] + '"' + '\n}'
 		f = open(f"{nvn_folder}{nvn_files[i]}{nvn_extension}", "w")
