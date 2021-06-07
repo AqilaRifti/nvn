970d1c086d9bd54cec159e7386e7a52c36850612
musangtara <musangtara@gmail.com>
2021-06-07 13:02:25 +0700

    initial commit

--- src/functions/init.py
+++ src/functions/init.py
@@ -10,12 +10,20 @@ except FileExistsError:
 p = pathlib.Path(foldername + "/" + ".nvnfile").mkdir(parents=True, exist_ok=True)
 
 def create_folder(foldername):
-	p = pathlib.Path(foldername).mkdir(parents=True, exist_ok=True)
+	try:
+		p = pathlib.Path(foldername).mkdir(parents=True)
+	except FileExistsError:
+		out.error("There is already folder with that name!")
 
-nvn_json = open(f"{foldername}/nvn.json", "w")
-nvn_config = open(f"{foldername}/.nvnfile/config.nvn.json", "w").write('{\n\t"name": "config"\n}')
-nvn_plugin = open(f"{foldername}/.nvnfile/plugins.nvn.json", "w").write('{\n\t"name": "plugins"\n}')
-nvn_cache = open(f"{foldername}/.nvnfile/cache.nvn.json", "w").write('{\n\t"name": "cache"\n}')
+def create_nvn_file():
+	nvn_json = open(f"{foldername}/nvn.json", "w")
 
-nvn_json.write('')
-nvn_json.close()
+#nvn_config = open(f"{foldername}/.nvnfile/config.nvn.json", "w").write('{\n\t"name": "config"\n}')
+#nvn_plugin = open(f"{foldername}/.nvnfile/plugins.nvn.json", "w").write('{\n\t"name": "plugins"\n}')
+#nvn_cache = open(f"{foldername}/.nvnfile/cache.nvn.json", "w").write('{\n\t"name": "cache"\n}')
+#
+#
+#nvn_json = open(f"{foldername}/nvn.json", "w")
+#nvn_config = open(f"{foldername}/.nvnfile/config.nvn.json", "w").write('{\n\t"name": "config"\n}')
+#nvn_plugin = open(f"{foldername}/.nvnfile/plugins.nvn.json", "w").write('{\n\t"name": "plugins"\n}')
+#nvn_cache = open(f"{foldername}/.nvnfile/cache.nvn.json", "w").write('{\n\t"name": "cache"\n}')
